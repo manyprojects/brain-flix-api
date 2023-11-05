98bd931c3791793333d05953774b2fa2290b5ab5
kai <kwen2921@gmail.com>
2023-11-05 09:53:49 -0500

    fetches undetailed video data from heroku api

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
 // initialize Express in project
 const express = require('express');
 const app = express();
-const  PORT =  process.env.PORT ?? 8080;
+const PORT =  process.env.PORT ?? 8080;
 const cors = require('cors');
+const axios = require('axios');
+const fs = require('fs');
+const apiUrl = 'https://project-2-api.herokuapp.com';
+const key = "966263aa-7639-4571-a879-005ec98839e3";
+const dataFilePath = './data/video.json';
 
 app.use(cors({
     origin: process.env.CLIENT_URL
 }));
+
+app.get('/fetch', async (req, res) => {
+    try {
+        const response = await axios.get(`${apiUrl}/videos?api_key=${key}`);
+        console.log(response.data);
+        fs.writeFileSync(dataFilePath, JSON.stringify(response.data, null, 2));
+        res.status(200).json({ message: 'Data fetched from heroku...' });
+    } catch (err) {
+        console.log('data fetching failed from heroku:', err);
+        res.status(500).json({ message: 'Data fetching unsuccessful from heroku...' });
+    }
+});
+
+
 
 
 app.use(express.json());
