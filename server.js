ce027eb80fe059771075a5ea1b8fecaf733f628c
kai <kwen2921@gmail.com>
2023-11-05 09:03:52 -0500

    folder structure

--- server.js
+++ server.js
@@ -2,6 +2,19 @@
 const express = require('express');
 const app = express();
 const  PORT =  process.env.PORT ?? 8080;
+const cors = require('cors');
+
+app.use(cors({
+    origin: process.env.CLIENT_URL
+}));
+
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+    res.send('API is running');
+});
+
 
 // start Express on port 8080
 app.listen(PORT, () => {
